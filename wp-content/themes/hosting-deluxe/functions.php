8f9213954dd427d71261a1ca4cf20d6442229777
Mwapsam <mwapsam@gmail.com>
2023-11-19 08:00:52 +0200

    Set locally

--- wp-content/themes/hosting-deluxe/functions.php
+++ wp-content/themes/hosting-deluxe/functions.php
@@ -1044,3 +1044,13 @@ function hostingdeluxe_custom_logo_setup() {
 }
 add_action( 'after_setup_theme', 'hostingdeluxe_custom_logo_setup' );
 
+add_action( 'wp_enqueue_scripts', 'my_enqueue_theme_js' );
+function my_enqueue_theme_js() {
+  wp_enqueue_script(
+    'my-theme-frontend',
+    get_stylesheet_directory_uri() . '/build/index.js',
+    ['wp-element'],
+    time(), // Change this to null for production
+    true
+  );
+}
